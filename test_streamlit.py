028e6a36d4d2d139fccb6f9b6c156eb50f1e939a
Prajwal D. Vishwanath <43853402+ParzivalDV@users.noreply.github.com>
2023-10-19 15:29:27 +0530

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -72,6 +72,3 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write("Uploaded file data:")
     st.dataframe(df)
-
-# Show a map
-st.map(np.random.randn(1000, 2) / [50, 50
