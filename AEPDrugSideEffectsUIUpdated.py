57536e3c2fa7c43d0a5f53e95551a64a152e23d3
Prajwal D. Vishwanath <43853402+ParzivalDV@users.noreply.github.com>
2023-10-20 14:08:11 +0530

    Update AEPDrugSideEffectsUIUpdated.py

--- AEPDrugSideEffectsUIUpdated.py
+++ AEPDrugSideEffectsUIUpdated.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import sklearn
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import time
